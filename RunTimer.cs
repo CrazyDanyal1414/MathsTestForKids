ec28544df8f5d24ffe67041ccdb05467e7970f11
danyal <danyal.qureshi1414@gmail.com>
2020-07-13 15:56:02 +0800

    fixing timer glitch

--- RunTimer.cs
+++ RunTimer.cs
@@ -8,37 +8,39 @@ namespace MathsTest
     public class RunTimer
     {
 		public bool IsTimeLeft { get; private set; } = true;
-		static readonly CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
-        readonly CancellationToken cancellationToken = cancellationTokenSource.Token;
 		public static void Timer(int numberOfSeconds, CancellationToken cancellationToken)
 		{
 			var whenToStop = DateTime.Now.AddSeconds(numberOfSeconds);
-            while (!cancellationToken.IsCancellationRequested)
-            {
-				while (DateTime.Now < whenToStop)
-				{
-					string timeLeft = (whenToStop - DateTime.Now).ToString(@"hh\:mm\:ss");
-					WriteToScreen($"Time Remaining: {timeLeft}", true);
-					Thread.Sleep(1000);
-				}
+			while (DateTime.Now < whenToStop && !cancellationToken.IsCancellationRequested)
+			{
+				string timeLeft = (whenToStop - DateTime.Now).ToString(@"hh\:mm\:ss");
+				WriteToScreen($"Time Remaining: {timeLeft}", true);
+				Thread.Sleep(1000);
 			}
 		}
 
+		static CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+		readonly CancellationToken cancellationToken = cancellationTokenSource.Token;
 		public Task timerTask;
 		public RunTimer(int numberOfSeconds)
 		{
 			timerTask = Task.Run(() =>
 			{
-				Timer(numberOfSeconds, cancellationToken);
+				while (!cancellationToken.IsCancellationRequested)
+                {
+					Timer(numberOfSeconds, cancellationToken);
+				}
 				timerTask = null;
 				IsTimeLeft = false;
+				cancellationTokenSource.Dispose();
+				cancellationTokenSource = new CancellationTokenSource();
 			}, cancellationToken);
 		}
 		public void StopTimer(int numberOfQuestionsLeft)
 		{
 			if(numberOfQuestionsLeft == 0)
             {
-				cancellationToken.ThrowIfCancellationRequested();
+				cancellationTokenSource.Cancel();
 			}
 		}
 	}
