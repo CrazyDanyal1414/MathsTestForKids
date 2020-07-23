36563f3d590d2de06b038f5101a3a0cb389a6930
danyal <danyal.qureshi1414@gmail.com>
2020-07-23 10:01:06 +0800

    unittesting

---
+++ UnitTests.cs
@@ -0,0 +1,41 @@
+using System.Threading;
+using NUnit.Framework;
+
+namespace MathsTest
+{
+    [TestFixture]
+    public class NUnitTest
+    {
+        [TestCase]
+        public void CheckIfGetPossibleOperationsByDifficultyWorks()
+        {
+            var (operationmin, operationmax) = Calculation.GetPossibleOperationsByDifficulty(UserDifficulty.Easy);
+            Assert.AreEqual(1, operationmin);
+            Assert.AreEqual(4, operationmax);
+        }
+
+        [TestCase]
+        public void TimerCancellationRequested()
+        {
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+            CancellationToken cancellationToken = cancellationTokenSource.Token;
+            Program.RunTest(0, UserDifficulty.Easy, 30);
+            if (cancellationToken.IsCancellationRequested)
+            {
+                Assert.That(cancellationToken.IsCancellationRequested);
+            }
+            else
+            {
+                Assert.That(!cancellationToken.IsCancellationRequested);
+            }
+        }
+
+        [TestCase]
+        public void CheckingIfSuggestingCorrectDifficulty()
+        {
+            UserDifficulty userDifficulty = CanUseManyTimes.SuggestingDifficulty("danyal");
+            Assert.AreEqual(UserDifficulty.Easy, userDifficulty);
+            
+        }
+    }
+}
